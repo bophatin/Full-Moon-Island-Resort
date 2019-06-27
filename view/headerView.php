17109e76a2b4a581e2b0fb44a15174280968f625
Bopha TIN <Bo@Bophas-iMac.local>
2019-06-27 20:35:05 +0200

    Ajout vue header

---
+++ view/headerView.php
@@ -0,0 +1,5 @@
+<div class="container_openNav">
+    <div id="openNav"><span></span></div>
+    <div class="logo"><a href="index.php">Full Moon Island</a></div>
+    <i class="far fa-moon"></i>
+</div>
