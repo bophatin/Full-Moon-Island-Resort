6541c15d5ac03fff7d27d25a62c5b249f4afb367
Bopha TIN <Bo@Bophas-iMac.local>
2019-06-27 20:36:10 +0200

    Ajout vue overlay

---
+++ view/navView.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="overlay" id="myNav">
+        <a href="#" id="closeNav">close</a>
+        <div class="container_overlay">
+            <nav class="overlay_content">
+                <ul>
+                    <li><a href="#">Villas</a></li>
+                    <li><a href="#">Bungalows</a></li>
+                    <li><a href="index.php?page=about">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
