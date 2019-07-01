3c3a2321ba447bb07f3d923896f9499aa5353968
Bopha TIN <Bo@Bophas-iMac.local>
2019-07-01 15:18:58 +0200

    Ajout background-img au container-txt

--- view/indexView.php
+++ view/indexView.php
@@ -2,11 +2,12 @@
 <?php $title = 'Full Moon Island Resort in Cambodia'; ?>
 
 <?php ob_start(); ?>
-    <div id="hp"></div>
-    <div id="container_txt" >
-        <p class="little_title">A unique experience in Cambodia</p>
-        <h1 id="h1_hp">An ecologic and luxury resort</h1>
-        <button>Discover</button>
+    <div id="hp">
+        <div id="container_txt" >
+            <p class="little_title">A unique experience in Cambodia</p>
+            <h1 id="h1_hp">An ecologic and luxury resort</h1>
+            <button>Discover</button>
+        </div>
     </div>
 <?php $content = ob_get_clean(); ?>
 
